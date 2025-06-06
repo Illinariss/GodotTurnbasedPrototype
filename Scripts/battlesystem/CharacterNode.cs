d0cf09951d9c4af02489f86a8a015e658f31130d
Illinariss <42944888+Illinariss@users.noreply.github.com>
2025-06-06 22:24:32 +0200

    Add character name label

--- Scripts/battlesystem/CharacterNode.cs
+++ Scripts/battlesystem/CharacterNode.cs
@@ -5,17 +5,19 @@ using System.Runtime.CompilerServices;
 public partial class CharacterNode : Node2D
 {
     [Export] public Texture2D CharacterImage;
-    private TextureRect _image;
+    private Sprite2D _image;
     private ProgressBar _hpBar;
     private ProgressBar _manaBar;
+    private Label _nameLabel;
 
     public CharacterData Data;
 
     public override void _Ready()
     {
-        _image = GetNode<TextureRect>("%CharacterTexture");
+        _image = GetNode<Sprite2D>("%CharacterTexture");
         _hpBar = GetNode<ProgressBar>("%HPBar");
         _manaBar = GetNode<ProgressBar>("%ManaBar");
+        _nameLabel = GetNode<Label>("%NameLabel");
 
         if (CharacterImage != null)
             _image.Texture = CharacterImage;
@@ -37,6 +39,8 @@ public partial class CharacterNode : Node2D
         _hpBar.Value = Data.CurrentHP;
         _manaBar.MaxValue = Data.MaxMana;
         _manaBar.Value = Data.CurrentMana;
+        if (_nameLabel != null)
+            _nameLabel.Text = Data.Name;
     }
 
     public static CharacterNode Create(CharacterData data)
